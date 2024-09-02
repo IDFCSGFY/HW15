19cca84b8f0b266572b23bc59cde0a15cc8c8af0
IDFCSGFY <idfcsgfy@gmail.com>
2024-09-03 03:23:37 +0700

    hw15

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,14 @@
-public class Bicycle {
+public class Bicycle extends TransportationDevice implements ServiceStation {
 
-    public String modelName;
-    public int wheelsCount;
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
     }
 }
--- src/Truck.java
+++ src/Truck.java
@@ -1,17 +1,20 @@
-public class Truck {
+public class Truck extends Car implements ServiceStation {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
+        this.checkEngine();
+        this.checkTrailer();
     }
 }
